97056b8912e6646b1f676f2d0af52a6003207c5d
ABhehe123 <mredadul@gmail.com>
2026-04-15 20:42:49 +0600

    Fix syntax and update comments in preload.js
    Fixed syntax issues and updated comments in preload.js.

--- src/main/preload.js
+++ src/main/preload.js
@@ -6,20 +6,16 @@ contextBridge.exposeInMainWorld('electronAPI', {
     // --- RESTORED: STANDARD DOWNLOAD ---
     downloadMod: (url) => ipcRenderer.send('download-file', url),
     
-    // --- DIRECT DOWNLOAD TO .MINECRAFT ---
-    downloadModDirect: (url, filename) => ipcRenderer.invoke('download-mod-direct', url, filename),
+    // --- DIRECT DOWNLOAD TO .Mblalalalalllala--
+    filename),
     
     // LAUNCHER IPC METHODS
-    launchMinecraft: (options) => ipcRenderer.send('launch-minecraft', options),
-    onLaunchProgress: (callback) => ipcRenderer.on('launch-progress', (event, data) => callback(data)),
-    onLaunchComplete: (callback) => ipcRenderer.on('launch-complete', () => callback()),
+
     onLaunchError: (callback) => ipcRenderer.on('launch-error', (event, error) => callback(error)),
 
 // LOCAL MOD INSPECTOR METHODS
     getLocalMods: () => ipcRenderer.invoke('get-local-mods'),
-    getTrashMods: () => ipcRenderer.invoke('get-trash-mods'),
-    moveToTrash: (filename) => ipcRenderer.invoke('move-to-trash', filename),
-    recoverFromTrash: (filename) => ipcRenderer.invoke('recover-from-trash', filename),
+    getTrashMods: er-from-trash', filename),
     
     // ✨ NEW: Opens the physical folder on your PC
     openModsFolder: () => ipcRenderer.send('open-mods-folder'),
